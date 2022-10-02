bd4c66522991206dfd07af0cc8d62d630071a0d6
Luis Angel Perez Melendez <angelmelen88@gmail.com>
2022-10-02 16:14:52 -0500

    java y python

--- cursoPOOuber/Java/Main.java
+++ cursoPOOuber/Java/Main.java
@@ -3,5 +3,16 @@ package Java;
 class Main {
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
+        Car car = new Car();
+        car.license = "AMQ123";
+        car.driver = "andres Herrera";
+        car.passegenger = 4;
+        car.printDataCar();
+        
+        Car car2 = new Car();
+        car2.license = "QWE567";
+        car2.driver = "Andrea Herrera";
+        car2.passegenger = 3;
+        car2.printDataCar();
     }
 }
